edd471e3c77e9a218f7e0ed3bd0ac8e558e1803b
Joanerson Costa <joanersonc31@gmail.com>
2024-12-26 13:43:03 -0300

    feat: inicia projeto

---
+++ card-management-service/servico-cliente/src/main/java/com/github/joanersoncosta/card_management_service/CardManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.joanersoncosta.card_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardManagementServiceApplication.class, args);
+	}
+
+}
