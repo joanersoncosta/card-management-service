05b5227cdb25e05979fd9dba6388991411b57905
Joanerson Costa <joanersonc31@gmail.com>
2024-12-27 14:13:34 -0300

    feat: implementa domain

--- card-management-service/servico-cliente/src/main/java/com/github/joanersoncosta/servico_cliente/cliente/domain/Cliente.java
+++ card-management-service/servico-cliente/src/main/java/com/github/joanersoncosta/servico_cliente/cliente/domain/Cliente.java
@@ -5,17 +5,35 @@ import java.util.UUID;
 
 import com.github.joanersoncosta.servico_cliente.cliente.application.api.request.ClienteNovoRequest;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
+@Entity
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "cliente", uniqueConstraints = {@UniqueConstraint(columnNames = "cpf"), @UniqueConstraint(columnNames = "email")})
 public class Cliente {
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(columnDefinition = "uuid", name = "id_cliente")
 	private UUID idCliente;
+    @Column(name = "nome")
 	private String nome;
+    @Column(name = "cpf", nullable = false, unique = true)
 	private String cpf;
+    @Column(name = "email")
 	private String email;
+    @Column(name = "idade")
 	private Integer idade;
+    @Column(name = "data_cadastro")
     private LocalDateTime dataCadastro;
 
 	public Cliente(ClienteNovoRequest clienteRequest) {
